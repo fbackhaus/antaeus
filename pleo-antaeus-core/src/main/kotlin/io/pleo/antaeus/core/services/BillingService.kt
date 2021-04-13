557524a3d4eab5f4f1ae13fda346e279fcfba613
Federico Backhaus <fbackhaus94@gmail.com>
2021-04-14 00:15:08 +0100

    Added BillingService function that pays pending invoices

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,9 +1,19 @@
 package io.pleo.antaeus.core.services
 
 import io.pleo.antaeus.core.external.PaymentProvider
+import io.pleo.antaeus.models.Invoice
+import io.pleo.antaeus.models.InvoiceStatus
 
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
-// TODO - Add code e.g. here
+
+    fun payPendingInvoices(invoices: List<Invoice>) {
+        for (invoice in invoices) {
+            if (invoice.status === InvoiceStatus.PENDING) {
+                paymentProvider.charge(invoice)
+                //TODO: How do we change the invoice status to paid?
+            }
+        }
+    }
 }

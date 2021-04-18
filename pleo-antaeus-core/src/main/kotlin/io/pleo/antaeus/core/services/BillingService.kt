9539ffea92285f9c4a3f8e2f9f2460eb63a7f45f
Federico Backhaus <fbackhaus94@gmail.com>
2021-04-18 21:25:00 +0100

    Added try/catch to handle exceptions when paying invoices and added logic to retrieve pending invoices

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -3,17 +3,38 @@ package io.pleo.antaeus.core.services
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
+import mu.KotlinLogging
 
 class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
+    private val logger = KotlinLogging.logger {}
 
-    fun payPendingInvoices(invoices: List<Invoice>) {
-        for (invoice in invoices) {
-            if (invoice.status === InvoiceStatus.PENDING) {
+    fun payPendingInvoices(pendingInvoices: List<Invoice>) {
+        val processedInvoices: MutableList<Invoice> = mutableListOf()
+
+        for (invoice in pendingInvoices) {
+            try {
                 paymentProvider.charge(invoice)
-                //TODO: How do we change the invoice status to paid?
+                processedInvoices.add(markInvoiceAsPaid(invoice))
+            } catch (e: Exception) {
+                logger.error(e) { "Exception thrown when trying to pay invoice" }
+                processedInvoices.add(invoice)
             }
+            //TODO: Retry when there's a NetworkException
+            //TODO: Update invoices in the database
+            //TODO: Send email/message to notify that the process has run, and what's the outcome
         }
     }
+
+    private fun markInvoiceAsPaid(invoice: Invoice): Invoice {
+        return Invoice(
+            invoice.id,
+            invoice.customerId,
+            invoice.amount,
+            InvoiceStatus.PAID
+        )
+    }
+
+
 }

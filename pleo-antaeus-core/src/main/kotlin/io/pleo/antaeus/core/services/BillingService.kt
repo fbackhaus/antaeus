7db0e5c567a0c1ba1a0f79736b2d856b8fa8711c
Federico Backhaus <fbackhaus94@gmail.com>
2021-04-19 19:43:23 +0100

    Added retry logic when a network exception happens

--- pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
+++ pleo-antaeus-core/src/main/kotlin/io/pleo/antaeus/core/services/BillingService.kt
@@ -1,5 +1,6 @@
 package io.pleo.antaeus.core.services
 
+import io.pleo.antaeus.core.exceptions.NetworkException
 import io.pleo.antaeus.core.external.PaymentProvider
 import io.pleo.antaeus.models.Invoice
 import io.pleo.antaeus.models.InvoiceStatus
@@ -9,22 +10,38 @@ class BillingService(
     private val paymentProvider: PaymentProvider
 ) {
     private val logger = KotlinLogging.logger {}
+    private val maxNumberOfRetries = 3
 
     fun payPendingInvoices(pendingInvoices: List<Invoice>) {
         val processedInvoices: MutableList<Invoice> = mutableListOf()
 
         for (invoice in pendingInvoices) {
-            try {
-                paymentProvider.charge(invoice)
-                processedInvoices.add(markInvoiceAsPaid(invoice))
-            } catch (e: Exception) {
-                logger.error(e) { "Exception thrown when trying to pay invoice" }
-                processedInvoices.add(invoice)
-            }
-            //TODO: Retry when there's a NetworkException
+            val processedInvoice = payInvoice(invoice)
+            processedInvoices.add(processedInvoice)
             //TODO: Update invoices in the database
             //TODO: Send email/message to notify that the process has run, and what's the outcome
         }
+    }
+
+    private fun payInvoice(invoice: Invoice, retries: Int = 1): Invoice {
+        try {
+            val successfulCharge = paymentProvider.charge(invoice)
+            if (successfulCharge) {
+                return markInvoiceAsPaid(invoice)
+            }
+        } catch (e: NetworkException) {
+            logger.warn(e) { "Network issue when trying to pay invoice, retries: $retries" }
+            if (shouldRetryPayment(retries)) {
+                return payInvoice(invoice, retries + 1)
+            }
+        } catch (e: Exception) {
+            logger.error(e) { "Exception thrown when trying to pay invoice" }
+        }
+        return invoice
+    }
+
+    private fun shouldRetryPayment(retries: Int): Boolean {
+        return retries < maxNumberOfRetries
     }
 
     private fun markInvoiceAsPaid(invoice: Invoice): Invoice {
@@ -35,6 +52,4 @@ class BillingService(
             InvoiceStatus.PAID
         )
     }
-
-
 }
